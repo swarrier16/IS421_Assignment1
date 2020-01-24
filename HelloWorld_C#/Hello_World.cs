c7b5173cb155bf8488f4007599a8c25e7934cef5
Shruti Warrier <swarrier@shrutis-mbp.resource.campus.njit.edu>
2020-01-23 20:11:35 -0500

    added C# files

---
+++ HelloWorld_C#/Hello_World.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace HelloWorld
+{
+    public class Hello_World
+    {
+        public Hello_World()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ HelloWorld_C#/MyClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace HelloWorld
+{
+    public class MyClass
+    {
+        public MyClass()
+        {
+        }
+    }
+}
